f142703ed7c47cd19e2c1a18deb50f25f888e6d2
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-05 19:42:32 +0530

    Basic CRUD for project routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,102 @@
 const router = require("express").Router();
+const Project = require("../models/project");
 
 router.route("/")
   .get((req, res) => {
-    res.send("Getting Projects...");
+    Project.find()
+      .then((documents) => {
+        res.status(200).json({
+          message: "Projects fetched succcessfully",
+          data: documents,
+          errors: null,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "Error fetching projects.",
+          document: null,
+          errors: error,
+        });
+      });
+  })
+
+  .post((req, res) => {
+    const project = req.body;
+
+    return Project.create(project)
+      .then((document) => {
+        res.status(201).json({
+          message: "Created a project successfully.",
+          data: document,
+          errors: null,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "Error creating project.",
+          data: null,
+          errors: error,
+        });
+      });
+  });
+
+router.route("/:uid")
+  .get((req, res) => {
+    Project.findOne({ uid: req.params.uid })
+      .then((document) => {
+        if (!document) {
+          throw Error("Project not found");
+        }
+        res.status(200).json({
+          message: "Fetched project successfully.",
+          data: document,
+          errors: null,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "Error fetching project",
+          data: null,
+          errors: error,
+        });
+      });
+  })
+
+  .patch((req, res) => {
+    const project = req.body;
+    Project.findOneAndUpdate({ uid: req.params.uid }, { ...project }, { new: true })
+      .then((document) => {
+        res.status(201).json({
+          message: "Project updated succcessfully.",
+          data: document,
+          errors: null,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "Error updating project.",
+          data: null,
+          errors: error,
+        });
+      });
+  })
+
+  .delete((req, res) => {
+    Project.deleteOne({ uid: req.params.uid })
+      .then((document) => {
+        res.status(200).json({
+          message: "Deleted project successfully.",
+          data: document,
+          errors: null,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "Error deleting project",
+          data: null,
+          errors: error,
+        });
+      });
   });
 
 module.exports = router;
