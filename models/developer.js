2964c7dd503950a8d6261e47d46efb05d871e0ed
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-05 16:37:10 +0530

    Basic Developer register done

--- models/developer.js
+++ models/developer.js
@@ -11,7 +11,7 @@ const developerSchema = new mongoose.Schema(
       required: true,
     },
     contact: {
-      type: String,
+      type: Number,
       required: true,
     },
     qualification: {
--- routes/developers.js
+++ routes/developers.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const Developer = require("../models/developer");
 
 // initializing the router with express router
 const router = express.Router();
 
 router.route("/")
   .get((req, res) => res.send("Getting developers.."));
+
+router.route("/auth/register")
+  .post((req, res) => {
+    const { developer } = req.body;
+    console.log("req.body --->", developer);
+
+    return Developer.create(req.body)
+      .then((document) => res.status(201).json({
+        message: "Developer created successfully.",
+        data: document,
+        errors: null,
+      }))
+      .catch((error) => res.status(422).json({
+        message: "Error creating developer.",
+        data: null,
+        errors: error,
+      }));
+  });
 
 module.exports = router;
