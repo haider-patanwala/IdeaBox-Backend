8561ed8ef064711b60a7a80a2fd3e8cac3ad8acb
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-05 19:07:47 +0530

    CRUD routes for developer

--- routes/developers.js
+++ routes/developers.js
@@ -9,10 +9,9 @@ router.route("/")
 
 router.route("/auth/register")
   .post((req, res) => {
-    const { developer } = req.body;
-    console.log("req.body --->", developer);
+    const developer = req.body;
 
-    return Developer.create(req.body)
+    return Developer.create(developer)
       .then((document) => res.status(201).json({
         message: "Developer created successfully.",
         data: document,
@@ -25,4 +24,65 @@ router.route("/auth/register")
       }));
   });
 
+router.route("/:uid")
+  .get((req, res) => {
+    Developer.findOne({ uid: req.params.uid })
+      .then((document) => {
+        if (!document) {
+          throw Error("Developer doesn't exist");
+        }
+        res.status(200).json({
+          message: "Developer fetched",
+          data: document,
+          error: null,
+        });
+      })
+      .catch((error) => res.status(400).json({
+        message: "Error fetching developer",
+        data: null,
+        errors: error,
+      }));
+  })
+
+  .patch((req, res) => {
+    const developer = req.body;
+
+    // respone bydefault comes an old document so giving new:true option to get a fresh updated document.
+    Developer.findOneAndUpdate({ uid: req.params.uid }, { ...developer }, { new: true })
+      .then((document) => {
+        if (!document) {
+          throw Error("Developer not found.");
+        }
+
+        return res.status(201).json({
+          message: "Developer Updated",
+          data: document,
+          error: null,
+        });
+      })
+      .catch((error) => res.status(422).json({
+        message: "Error updating developer.",
+        data: null,
+        errors: error,
+      }));
+  })
+
+  .delete((req, res) => {
+    Developer.deleteOne({ uid: req.params.uid })
+      .then((document) => {
+        if (document.deletedCount === 0) {
+          throw Error("Developer not found.");
+        }
+        return res.status(200).json({
+          message: "Developer deleted successfully.",
+          data: document,
+          error: null,
+        });
+      })
+      .catch((error) => res.status(400).json({
+        message: "Error deleting developer.",
+        data: null,
+        errors: error,
+      }));
+  });
 module.exports = router;
