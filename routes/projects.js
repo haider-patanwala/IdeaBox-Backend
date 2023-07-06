35ba1782882f3f01d30e322777d9b1e906811eb9
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-06 16:51:37 +0530

    Added errorHandler

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const Project = require("../models/project");
+const ApiError = require("../utiis/ApiError");
 
 router.route("/")
-  .get((req, res) => {
+  .get((req, res, next) => {
     Project.find()
       .then((documents) => {
         res.status(200).json({
@@ -11,16 +12,10 @@ router.route("/")
           errors: null,
         });
       })
-      .catch((error) => {
-        res.status(400).json({
-          message: "Error fetching projects.",
-          document: null,
-          errors: error,
-        });
-      });
+      .catch((error) => next(new ApiError(400, "Error fetching projects.", error.toString())));
   })
 
-  .post((req, res) => {
+  .post((req, res, next) => {
     const project = req.body;
 
     return Project.create(project)
@@ -31,17 +26,11 @@ router.route("/")
           errors: null,
         });
       })
-      .catch((error) => {
-        res.status(422).json({
-          message: "Error creating project.",
-          data: null,
-          errors: error,
-        });
-      });
+      .catch((error) => next(new ApiError(422, "Error creating project.", error.toString())));
   });
 
 router.route("/:uid")
-  .get((req, res) => {
+  .get((req, res, next) => {
     Project.findOne({ uid: req.params.uid })
       .then((document) => {
         if (!document) {
@@ -53,50 +42,41 @@ router.route("/:uid")
           errors: null,
         });
       })
-      .catch((error) => {
-        res.status(400).json({
-          message: "Error fetching project",
-          data: null,
-          errors: error,
-        });
-      });
+      .catch((error) => next(new ApiError(400, "Error fetching project.", error.toString())));
   })
 
-  .patch((req, res) => {
+  .patch((req, res, next) => {
     const project = req.body;
     Project.findOneAndUpdate({ uid: req.params.uid }, { ...project }, { new: true })
       .then((document) => {
+        if (!document) {
+          throw Error("Project not found.");
+        }
         res.status(200).json({
           message: "Project updated succcessfully.",
           data: document,
           errors: null,
         });
       })
-      .catch((error) => {
-        res.status(422).json({
-          message: "Error updating project.",
-          data: null,
-          errors: error,
-        });
-      });
+      .catch((error) => next(new ApiError(422, "Error updating project.", error.toString())));
   })
 
-  .delete((req, res) => {
+  .delete((req, res, next) => {
     Project.deleteOne({ uid: req.params.uid })
       .then((document) => {
+        // deleteOne method doesnt return the found/deleted document
+        // but it returns a key `deletedCount` with value 0 or 1
+        // so if it is 0 means nothing was deleted means the documen was not found to delete.
+        if (!document.deletedCount) {
+          throw Error("Project not found.");
+        }
         res.status(200).json({
           message: "Deleted project successfully.",
           data: document,
           errors: null,
         });
       })
-      .catch((error) => {
-        res.status(400).json({
-          message: "Error deleting project",
-          data: null,
-          errors: error,
-        });
-      });
+      .catch((error) => next(new ApiError(400, "Error deleting project.", error.toString())));
   });
 
 module.exports = router;
