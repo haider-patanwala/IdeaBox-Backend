d4c0a6fa209f2f7c5b5be9c147bb2cdfd70dd4db
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-09 15:37:02 +0530

    Added a POST controller

--- routes/developers.js
+++ routes/developers.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const { promisify } = require("util");
 const cloudinary = require("cloudinary").v2;
+const { body } = require("express-validator");
 const Developer = require("../models/developer");
 const ApiError = require("../utils/ApiError");
-const { deleteTmp } = require("../utils/deleteTmp");
+// const { deleteTmp } = require("../utils/deleteTmp");
+const controller = require("../controllers/developer");
 
 router.route("/")
   .get((req, res, next) => {
@@ -20,35 +22,31 @@ router.route("/")
   });
 
 router.route("/auth/register")
-  .post((req, res, next) => {
+  .post(body("developer.password").isLength({ min: 8, max: 16 }), (req, res, next) => {
     const developer = req.body;
-    const file = req.files.photo;
+    const file = req.files ? req.files.photo : null;
 
     try {
-      const cloudinaryUpload = promisify(cloudinary.uploader.upload);
-      return cloudinaryUpload(file.tempFilePath)
-        .then((result) => {
-          developer.profile_pic = result.url;
-          return Developer.create(developer);
-        })
-        .then((document) => {
-          res.status(201).json({
-            message: "Developer created successfully.",
-            data: document,
-            errors: null,
+      if (file) {
+        const cloudinaryUpload = promisify(cloudinary.uploader.upload);
+        return cloudinaryUpload(file.tempFilePath)
+          .then((result) => {
+            developer.profile_pic = result.url;
+            // return Developer.create(developer);
+            controller.registerDeveloper(res, next, developer, file);
+          })
+          .catch((error) => {
+            // console.log("Error --", error);
+            // *** this if condition is for cloudinaryUpload(file.tempFilePath) promise as it returns error in object form with key `http_code` over here so handling it accordingly for that specific argument of tempFilePath
+            // a typo in `tempFilePath` spelling will trigger satisfy this `if` block.
+            if (error.http_code) {
+              next(new ApiError(422, "Error creating developer!!", JSON.stringify(error)));
+            } else { // this error block is for handling errors of the then block to handle any error occured before passing the execution to the controller.
+              next(new ApiError(422, "Error creating developer!", error.toString()));
+            }
           });
-          deleteTmp(file);
-        })
-        .catch((error) => {
-          // console.log("Error --", error);
-          // this if condition is for cloudinaryUpload(file.tempFilePath) promise as it returns error in object form with key `http_code` over here so handling it accordingly for that specific argument of tempFilePath
-          // a typo in `tempFilePath` spelling will trigger satisfy this `if` block.
-          if (error.http_code) {
-            next(new ApiError(422, "Error creating developer!", JSON.stringify(error)));
-          } else {
-            next(new ApiError(422, "Error creating developer.", error.toString()));
-          }
-        });
+      }
+      return controller.registerDeveloper(res, next, developer, file);
     } catch (error) {
       return next(new ApiError(422, "Error creating developer..", error.toString()));
     }
