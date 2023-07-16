7e554a2e8d87f3ffcda0e588dca09e36aecb54f4
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-16 18:13:04 +0530

    Added comments and updated HTTP status codes

--- routes/organizations.js
+++ routes/organizations.js
@@ -24,6 +24,7 @@ router.route("/")
 
     // destructuring query keys from URL.
     const { name, domain, sort } = req.query;
+    // req.query helps for finding only those specific documents which are queried from the URL like /organizations?name=raw&domain=IT
 
     if (name) { // FOR CASE-INSENSITIVE SEARCHING
       queryObject.name = { $regex: name, $options: "i" };
@@ -32,17 +33,23 @@ router.route("/")
       queryObject.domain = { $regex: domain, $options: "i" };
     }
 
+    // had to put the find method in a variable as we needed to put sort over it again.
+    // `populate` is used to fetch the foreign key referenced document in the find response based on the keys passed as an argument to the method.
     let fetchedData = Organization.find(queryObject).populate("org_projects");
 
+    // if user has written `?sort=name,domain` with multiple sort conditions in URL :
     if (sort) { // FOR SORTING BASED ON ANY KEY
       const fixedSort = sort.replace(",", " ");
+      // Sorting is achieved by sort("name") function
       fetchedData = fetchedData.sort(fixedSort);
     }
 
+    // if no sort then do the work as usual
     fetchedData
       .then((documents) => {
         if (documents.length === 0) {
-          res.status(404).json({
+          // returns response of empty array with 'successful request' 200 code
+          res.status(200).json({
             message: "No organizations data found. Insert some data please.",
             data: documents,
             errors: null,
@@ -55,10 +62,12 @@ router.route("/")
           });
         }
       })
-      .catch((error) => next(new ApiError(400, "Error fetching organizations.", error.toString())));
+      .catch((error) => next(new ApiError(422, "Error fetching organizations.", error.toString())));
   });
 
 router.route("/auth/register")
+
+  // body() is an express-validator middleware
   .post(body("password").isLength({ min: 8, max: 16 }), async (req, res, next) => {
     const organization = req.body;
     const { password } = req.body;
@@ -67,6 +76,7 @@ router.route("/auth/register")
 
     if (!password) {
       const { msg, path } = errors.array()[0];
+      // 400 HTTP status code because the error would be related to invalid request payload and this is a severe error!
       return next(new ApiError(400, "Organization registration failed. Please provide a password.", `${path} : ${msg}`));
     }
     if (!errors.isEmpty()) {
@@ -79,6 +89,7 @@ router.route("/auth/register")
 
       const securedOrganization = { ...organization, password: securedPassword };
       if (file) {
+        // A promise was needed to handle the errors and process the result using then blocks so promisified the cloudinary method as it is not a promise by default.
         const cloudinaryUpload = promisify(cloudinary.uploader.upload);
         return cloudinaryUpload(file.tempFilePath)
           .then((result) => {
@@ -92,11 +103,12 @@ router.route("/auth/register")
           // a typo in `tempFilePath` spelling will trigger satisfy this `if` block.
             if (error.http_code) {
               next(new ApiError(422, "Error creating organization!", JSON.stringify(error)));
-            } else {
+            } else { // this error block is for handling errors of the then block to handle any error occured before passing the execution to the controller.
               next(new ApiError(422, "Error creating organization!!", error.toString()));
             }
           });
       }
+      // if the file is not sent in request then do normal operations
       return controller.postOrganization(res, next, securedOrganization, file);
     } catch (error) {
       return next(new ApiError(422, "Error creating organization..", error.toString()));
@@ -111,12 +123,14 @@ router.route("/auth/login")
         if (!document) {
           throw Error("Organization not found.");
         }
+        // this compare function returns a boolean value here.
         const passwordCompare = await bcrypt.compare(password, document.password);
         if (!passwordCompare) {
           throw Error("Incorrect Password.");
         }
 
         const token = jwt.sign({ uid }, secret);
+
         res.status(200).json({
           message: "Organization logged in successfully.",
           data: {
@@ -144,9 +158,10 @@ router.route("/:uid")
           errors: null,
         });
       })
-      .catch((error) => next(new ApiError(400, "Error fetching organization.", error.toString())));
+      .catch((error) => next(new ApiError(422, "Error fetching organization.", error.toString())));
   })
 
+  // isOrganizationAuthenticated is a middleware
   .patch(isOrganizationAuthenticated, (req, res, next) => {
     const organization = req.body;
     const file = req.files ? req.files.photo : null;
@@ -171,12 +186,15 @@ router.route("/:uid")
             }
           });
       } else {
+        // if the file is not sent in request then do normal operations
         controller.updateOrganiztion(req, res, next, organization, file);
       }
     } catch (error) {
       next(new ApiError(422, "Error updating organization..", error.toString()));
     }
   })
+
+  // isOrganizationAuthenticated is a middleware
   .delete(isOrganizationAuthenticated, (req, res, next) => {
     Organization.deleteOne({ uid: req.params.uid })
       .then((document) => {
--- routes/review.js
+++ routes/review.js
@@ -10,26 +10,42 @@ router.route("/")
 
     // destructuring query key from URL.
     const { sort } = req.query;
+    // req.query helps for finding only those specific documents which are queried from the URL like /reviews?sort=createdAt
 
+    // had to put the find method in a variable as we needed to put sort over it again.
+    // `populate` is used to fetch the foreign key referenced document in the find response based on the keys passed as an argument to the method.
     // populating specific collections with only selected properties. - means to neglect that field i.e. _id
     let fetchedData = Review.find(queryObject).populate({ path: "developer", select: "fname lname email uid -_id" }).populate({ path: "organization", select: "name website uid -_id" }).populate({ path: "project", select: "title uid -_id" });
 
-    // if user has written `?sort=fname,city` with multiple sort conditions in URL :
+    // if user has written `?sort=createdAt,updatedAt` with multiple sort conditions in URL :
     if (sort) {
       const sortFixed = sort.replace(",", " ");
+      // Sorting is achieved by sort("createdAt,updatedAt") function
       fetchedData = fetchedData.sort(sortFixed);
     }
 
+    // if no sort then do the work as usual
     fetchedData
       .then((documents) => {
-        res.status(200).json({
-          message: "Fetched reviews succesfully.",
-          data: documents,
-          errors: null,
-        });
+        if (documents.length === 0) {
+          // returns response of empty array with 'successful request' 200 code
+          res.status(200).json({
+            message: "No reviews data found. Insert some data please.",
+            data: documents,
+            errors: null,
+          });
+        } else {
+          res.status(200).json({
+            message: "Fetched reviews succesfully.",
+            data: documents,
+            errors: null,
+          });
+        }
       })
-      .catch((error) => next(new ApiError(400, "Error fetching reviews.", error.toString())));
+      .catch((error) => next(new ApiError(422, "Error fetching reviews.", error.toString())));
   })
+
+  // roleBasedAuthentication is a middlware powered by other 2 middlwares to conditionally verfy the authToken.
   .post(roleBasedAuthentication, (req, res, next) => {
     const review = req.body;
     Review.create(review)
@@ -44,6 +60,8 @@ router.route("/")
   });
 
 router.route("/:uid")
+
+  // roleBasedAuthentication is middlware powered by other 2 middlwares to conditionally verfy the authToken.
   .patch(roleBasedAuthentication, (req, res, next) => {
     const updatedReview = req.body;
 
@@ -60,6 +78,8 @@ router.route("/:uid")
       })
       .catch((error) => next(new ApiError(422, "Error updating review.", error.toString())));
   })
+
+  // roleBasedAuthentication is middlware powered by other 2 middlwares to conditionally verfy the authToken.
   .delete(roleBasedAuthentication, (req, res, next) => {
     Review.deleteOne({ uid: req.params.uid })
       .then((document) => {
@@ -72,7 +92,7 @@ router.route("/:uid")
           errors: null,
         });
       })
-      .catch((error) => next(new ApiError(400, "Error deleting review.", error.toString())));
+      .catch((error) => next(new ApiError(422, "Error deleting review.", error.toString())));
   });
 
 module.exports = router;
