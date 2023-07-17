85ab955622c7287e00c193eeb009037a5f05979e
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-18 00:46:01 +0530

    Tested PATCH, DELETE developer routes

--- test/routes/developer.test.js
+++ test/routes/developer.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest"); // for HTTP requests testing
 const { expect } = require("chai");
 const server = require("../../server");
-const developerModel = require("../../models/developer");
+const developerModel = require("../../models/developer"); // only for before and after hooks
 
 let api;
 let uid;
+let authToken;
 
 describe("Developer API", () => {
   before("Initialize API in before block", (done) => {
@@ -20,7 +21,7 @@ describe("Developer API", () => {
       {
         fname: "Meet",
         lname: "Makwana",
-        email: "meetm@gmail.com",
+        email: "meet@gmail.com",
         password: "12345678",
         phone: 9876543210,
         qualification: "BE in Computer",
@@ -44,15 +45,16 @@ describe("Developer API", () => {
       .catch(done);
   });
 
-  after("Purge the dummy data after testing is completed by the describe() interface", (done) => {
-    developerModel.deleteMany({})
-      .then(() => done())
-      .catch(done);
-  });
+  // SHIFTING THE PURGING LOGIC AT THE END OF DELETE ROUTE'S TEST CASE SINCE THIS GETS FIRED BEFORE PATCH/DELETE COMPLETES EXECUTION WITH THE ACCESS_TOKEN GENERATE BY PREVIOUS' POST OPERATION.
+  // DUE TO ASYNC NATURE OF AFTER(), IT DELETES THE DEVELOPER FIRST ONLY AND THE VERIFICATION OF ACCESS_TOKEN FAILS FOR THE CONSEQUENT PATCH/DELETE TEST CASES.
+  // after("Purge the dummy data after testing is completed by the describe() interface", async () => {
+  //   console.log("I SHOULD BE EXECUTED AFTER DELETE");
+  //   await developerModel.deleteMany({});
+  // });
 
   // test cases for GET all route
-  it("GET all developers", (done) => {
-    api
+  it("GET all developers", async () => {
+    await api
       .get("/developers")
       .then((response) => {
         expect(response.status).to.equal(200);
@@ -61,14 +63,12 @@ describe("Developer API", () => {
         expect(response.body).to.has.property("errors");
         expect(response.body.data).to.be.an("array");
         expect(response.body.errors).to.equal(null);
-        done();
-      })
-      .catch(done);
+      });
   });
 
   // test cases for POST route
-  it("POST a developer", (done) => {
-    api.post("/developers/auth/register")
+  it("POST a developer", async () => {
+    await api.post("/developers/auth/register")
       .attach("photo", "test/resources/developer.jpg")
       .field("fname", "Meet")
       .field("lname", "Makwana")
@@ -89,35 +89,64 @@ describe("Developer API", () => {
       .field("github", "http://github.com/meetmakwana19")
       .field("technical_role", "Full Stack Web Developer")
       .then((response) => {
-        uid = response.body.data.uid;
+        // saving uid and access_token for the next PATCH and DELETE request
+        authToken = response.body.data.access_token;
+        uid = response.body.data.developer.uid;
+        console.log("POST authToken is ----------", authToken);
+        console.log("POST uid is ----------", uid);
+        console.log("POSTED ----------", response.body);
+
         expect(response.status).to.equal(201);
 
-        expect(response.body).to.have.property("message");
-        expect(response.body).to.have.property("data", "Developer created successfully.");
+        expect(response.body).to.have.property("message", "Developer created successfully.");
+        expect(response.body).to.have.property("data");
         expect(response.body).to.have.property("errors", null);
         expect(response.body.data).to.have.property("access_token");
         expect(response.body.data).to.have.property("developer");
-
-        done();
-      })
-      .catch(done());
+      });
+    //  Allowing done() to be passed as a reference rather than invoking it immediately. like catch(done())
+    // This allows the console.log() to work properly in the above then block.
   });
+
   // test cases for PATCH route
-  it("PATCH a developer", (done) => {
-    api.patch(`/developers/${uid}`)
+  it("PATCH a developer", async () => {
+    await api.patch(`/developers/${uid}`)
+      .set("authorization", `${authToken}`)
       .attach("photo", "test/resources/developer.jpg")
       .field("qualification", "BTech in Computer")
       .field("city", "Delhi")
       .field("openToWork", false)
       .then((response) => {
+        console.log("PATCH token-----------", authToken);
+        console.log("PATCH uid-----------", uid);
+        console.log("UPDATED --------------", response.body);
         expect(response.status).to.equal(200);
 
         expect(response.body).to.have.property("message");
-        expect(response.body).to.have.property("data", "Developer updated successfully.");
+        expect(response.body).to.have.property("message", "Developer updated successfully.");
+        expect(response.body).to.have.property("errors", null);
+      });
+  });
+
+  // test cases for DELETE route
+  it("DELETE a developer", async () => {
+    console.log("DELETE token-----------", authToken);
+    console.log("DELETE uid-----------", uid);
+
+    const deletePromise = api.delete(`/developers/${uid}`)
+      .set("authorization", `${authToken}`)
+      .then(async (response) => {
+        console.log("DELETED-------------", response);
+        expect(response.status).to.equal(200);
+
+        expect(response.body).to.have.property("message");
+        expect(response.body).to.have.property("data", "Developer deleted successfully.");
         expect(response.body).to.have.property("errors", null);
 
-        done();
-      })
-      .catch(done());
+        await deletePromise;
+        await developerModel.deleteMany({});
+        // .then(() => done())
+        // .catch(done);
+      });
   });
 });
