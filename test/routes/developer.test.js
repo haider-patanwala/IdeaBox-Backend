afc3cd69a6e6768d722f7055e6a0d3f96d003679
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-19 02:51:55 +0530

    Covered 89% in testing developer CRUD

--- test/routes/developer.test.js
+++ test/routes/developer.test.js
@@ -2,12 +2,15 @@ const request = require("supertest"); // for HTTP requests testing
 const { expect } = require("chai");
 const server = require("../../server");
 const developerModel = require("../../models/developer"); // only for before and after hooks
+require("dotenv").config();
+
+const devAuthToken = process.env.DEV_AUTH_TOKEN;
 
 let api;
 let uid;
 let authToken;
 
-describe("Developer API", () => {
+describe.only("Developer API", () => {
   before("Initialize API in before block", (done) => {
     server
       .then((resultedApp) => {
@@ -70,6 +73,56 @@ describe("Developer API", () => {
       });
   });
 
+  // test cases for GET all route
+  it("GET all developers by searching city", async () => {
+    await api
+      .get("/developers?city=kan")
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.data[0]).to.has.property("city", "Kanpur");
+      });
+  });
+
+  // test cases for GET all route
+  it("GET all developers by searching fname", async () => {
+    await api
+      .get("/developers?fname=ank")
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.data[0]).to.has.property("fname", "Ankit");
+      });
+  });
+
+  // test cases for GET all route
+  it("GET all developers by searching lname", async () => {
+    await api
+      .get("/developers?lname=tiwa")
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.data[0]).to.has.property("lname", "Tiwari");
+      });
+  });
+
+  // test cases for GET all route
+  it("GET all developers by searching qualification", async () => {
+    await api
+      .get("/developers?qualification=m.sc")
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.data[0]).to.has.property("qualification", "M.Sc in Information Technology");
+      });
+  });
+
+  // test cases for GET all route
+  it("GET all developers by searching openToWork and sorting", async () => {
+    await api
+      .get("/developers?openToWork=true&sort=fname")
+      .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.data[0]).to.has.property("openToWork", true);
+      });
+  });
+
   // test cases for POST route
   it("POST a developer", (done) => {
     api.post("/developers/auth/register")
@@ -117,6 +170,108 @@ describe("Developer API", () => {
     // --------IGNORE till here
   });
 
+  // test cases for POST route
+  it("POST a developer without photo file", (done) => {
+    api.post("/developers/auth/register")
+      .field("fname", "Tony")
+      .field("lname", "Stark")
+      .field("email", "tony@gmail.com")
+      .field("password", "12345678")
+      .field("phone", "9876543210")
+      .then((response) => {
+        // saving uid and access_token for the next PATCH and DELETE request
+        // authToken = response.body.data.access_token;
+        // uid = response.body.data.developer.uid;
+        console.log("POSTED without photo----------", response.body);
+
+        expect(response.status).to.equal(201);
+
+        expect(response.body).to.have.property("message", "Developer created successfully.");
+        expect(response.body).to.have.property("data");
+        expect(response.body).to.have.property("errors", null);
+        expect(response.body.data).to.have.property("access_token");
+        expect(response.body.data).to.have.property("developer");
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for POST route
+  it("POST a developer without password", (done) => {
+    api.post("/developers/auth/register")
+      .field("fname", "Tony")
+      .field("lname", "Stark")
+      .field("email", "tony@gmail.com")
+      .then((response) => {
+        console.log("POSTED without pw----------", response.body);
+
+        expect(response.status).to.equal(400);
+
+        expect(response.body).to.have.property("message", "Developer registration failed. Please provide a password.");
+        expect(response.body).to.not.have.property("data");
+        expect(response.body).to.have.property("error", "password : Password should be atleast 8 and maximum 16 characters");
+
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for Login route
+  it("LOG IN  a developer", (done) => {
+    api.post("/developers/auth/login")
+      .field("email", "ankit@gmail.com")
+      .field("password", "12345678")
+      .then((response) => {
+        console.log("LOGIN----------", response.body);
+
+        expect(response.status).to.equal(200);
+
+        expect(response.body).to.have.property("message", "Developer login successful.");
+        expect(response.body).to.have.property("data");
+        expect(response.body).to.have.property("errors", null);
+        expect(response.body.data).to.have.property("access_token");
+        expect(response.body.data).to.have.property("developer");
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for Login route
+  it("LOG IN  a developer with wrong password", (done) => {
+    api.post("/developers/auth/login")
+      .field("email", "ankit@gmail.com")
+      .field("password", "12345677")
+      .then((response) => {
+        console.log("LOGIN wrong password----------", response.body);
+
+        expect(response.status).to.equal(422);
+
+        expect(response.body).to.have.property("message", "Error logging in Developer.");
+        expect(response.body).to.not.have.property("data");
+        expect(response.body).to.have.property("error", "Error: Incorrect Password.");
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for Login route
+  it("LOG IN  a developer with wrong email", (done) => {
+    api.post("/developers/auth/login")
+      .field("email", "anki@gmail.com")
+      .field("password", "12345678")
+      .then((response) => {
+        console.log("LOGIN wrong email----------", response.body);
+
+        expect(response.status).to.equal(422);
+
+        expect(response.body).to.have.property("message", "Error logging in Developer.");
+        expect(response.body).to.not.have.property("data");
+        expect(response.body).to.have.property("error", "Error: Developer not found.");
+        done();
+      })
+      .catch(done);
+  });
+
   // test cases for PATCH route
   it("PATCH a developer", (done) => {
     api.patch(`/developers/${uid}`)
@@ -139,11 +294,55 @@ describe("Developer API", () => {
       .catch(done);
   });
 
+  // test cases for PATCH route
+  it("PATCH a developer without photo", (done) => {
+    api.patch(`/developers/${uid}`)
+      .set("authorization", `${authToken}`)
+      .field("openToWork", false)
+      .then((response) => {
+        console.log("UPDATED without photo--------------", response.body);
+        expect(response.status).to.equal(200);
+
+        expect(response.body).to.have.property("message");
+        expect(response.body).to.have.property("message", "Developer updated successfully.");
+        expect(response.body).to.have.property("errors", null);
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for GET route
+  it("GET a specific developer", (done) => {
+    api.get(`/developers/${uid}`)
+      .then((response) => {
+        console.log("GOT --------------", response.body);
+        expect(response.status).to.equal(200);
+
+        expect(response.body).to.have.property("message");
+        expect(response.body).to.have.property("message", "Developer fetched successfully.");
+        expect(response.body).to.have.property("errors", null);
+        done();
+      })
+      .catch(done);
+  });
+
+  // test cases for GET route
+  it("GET a specific developer with wrong uid", (done) => {
+    api.get(`/developers/${uid}xx`)
+      .then((response) => {
+        console.log("GOT wrong --------------", response.body);
+        expect(response.status).to.equal(422);
+
+        expect(response.body).to.have.property("message");
+        expect(response.body).to.have.property("message", "Error fetching developer.");
+        expect(response.body).to.have.property("error", "Error: Developer not found");
+        done();
+      })
+      .catch(done);
+  });
+
   // test cases for DELETE route
   it("DELETE a developer", async () => {
-    console.log("DELETE token-----------", authToken);
-    console.log("DELETE uid-----------", uid);
-
     const deletePromise = api.delete(`/developers/${uid}`)
       .set("authorization", `${authToken}`)
       .then(async (response) => {
@@ -154,6 +353,40 @@ describe("Developer API", () => {
         expect(response.body).to.have.property("data");
         expect(response.body).to.have.property("message", "Developer deleted successfully.");
         expect(response.body).to.have.property("errors", null);
+      });
+    // remember to call the promise and delete method outside the promise definition over here
+    await deletePromise;
+
+    // await developerModel.deleteMany({});
+
+    // deleting just the recently added one which was inserted by our test suite.
+    // We need atleast one developer in test db as the access_token is used in the proposal.test.js & review.test.js
+    // if all documents are deleted then there would be nothing to verify the access_token used in proposal.test.js & review.test.js
+    await developerModel.findOneAndDelete({}, { sort: { _id: -1 } })
+      .then((result) => {
+        console.log("Deleting the recently added document------->", result);
+      })
+      .catch((error) => {
+        console.log("Error in deleting the recently added document------->", error);
+      });
+
+    // then and catch logic is not needed when using async/await
+    // .then(() => done())
+    // .catch(done);
+  });
+
+  // test cases for DELETE route
+  it("DELETE a developer with wrong uid", async () => {
+    const deletePromise = api.delete(`/developers/${uid}xx`)
+      .set("authorization", devAuthToken)
+      .then(async (response) => {
+        console.log("DELETED Developer-------------", response.body);
+
+        expect(response.status).to.equal(422);
+
+        expect(response.body).to.not.have.property("data");
+        expect(response.body).to.have.property("message", "Error deleting developer.");
+        expect(response.body).to.have.property("error", "Error: Developer not found.");
       });
     // remember to call the promise and delete method outside the promise definition over here
     await deletePromise;
